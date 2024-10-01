5b8420f8be5477f1d874421206a5b2c2bd76bca7
shubhamk922 <shubhamk922@gmail.com>
2024-10-01 19:52:11 +0530

    Commit 3 - add response body , add response headers

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 /*
@@ -15,12 +17,31 @@ func home(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("Welcome to Home"))
 }
 
-func snippetview(w http.ResponseWriter, _ *http.Request) {
-	w.Write([]byte("Welcome to Snippet View"))
+func snippetview(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+
+	if id < 1 || err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Welcome to Snippet View %d...", id)
+	//w.Write([]byte(fmt.Sprintf("Welcome to Snippet View %d", id)))
 }
 
 func snippetCreate(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("Welcome to Snippet Create"))
+}
+
+func snippetCreatePost(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Server", "Go") // Add all haeder maps
+
+	w.WriteHeader(http.StatusCreated) // status code should be set before writing response
+
+	// if you see writing response body there is a method Write that means
+	// http.ResponseWriter has Write method ie it implement io.Writer interface
+	// fmt.Fprint( w, "Hello")
+	// io.WriteString(w, "Hello")
+	w.Write([]byte("Save a new snippet")) // write the response
 }
 
 func main() {
@@ -30,8 +51,9 @@ func main() {
 	/*for the sake of clarity, maintainability and security, it’s generally a good idea to avoid http.DefaultServeMux and the corresponding helper functions. Use your own locally-scoped servemux instead, like we have been doing in this project so far.*/
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	mux.HandleFunc("/snippet/view", snippetview)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetview)
+	mux.HandleFunc("POST /snippet/view", snippetCreatePost)
 
 	err := http.ListenAndServe(":4000", mux)
 	//err = http.ListenAndServe(":4000", nil) This will use http.DefaultServerMux
